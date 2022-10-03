5082cc7bd1d407e8729a847562b61b242fc74023
Laurenz <git@laure.nz>
2022-10-03 12:58:56 +0200

    Switch to fuse3

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ apk update
 apk add alpine-sdk util-linux strace file autoconf automake libtool
 
 # Build static squashfuse
-apk add fuse-dev fuse-static zstd-dev zstd-static zlib-dev zlib-static # fuse3-static fuse3-dev
+apk add fuse3-static fuse3-dev zstd-dev zstd-static zlib-dev zlib-static
 wget -c -q "https://github.com/vasi/squashfuse/archive/e51978c.tar.gz"
 tar xf e51978c.tar.gz
 rm e51978c.tar.gz
@@ -21,7 +21,7 @@ cd squashfuse-*/
 ./autogen.sh
 ./configure --help
 ./configure CFLAGS=-no-pie LDFLAGS=-static
-make -j$(nproc)
+make -j
 make install
 /usr/bin/install -c -m 644 fuseprivate.h /usr/local/include/squashfuse
 cd -
@@ -30,11 +30,11 @@ rm -rf squashfuse-*
 # Build static AppImage runtime
 export GIT_COMMIT=$(cat src/runtime/version)
 cd src/runtime
-make runtime-fuse2 -j$(nproc)
-file runtime-fuse2
-strip runtime-fuse2
-ls -lh runtime-fuse2
-echo -ne 'AI\x02' | dd of=runtime-fuse2 bs=1 count=3 seek=8 conv=notrunc # magic bytes, always do AFTER strip
+make runtime-fuse3 -j
+file runtime-fuse3
+strip runtime-fuse3
+ls -lh runtime-fuse3
+echo -ne 'AI\x02' | dd of=runtime-fuse3 bs=1 count=3 seek=8 conv=notrunc # magic bytes, always do AFTER strip
 cd -
 
 # Use the same architecture names as https://github.com/AppImage/AppImageKit/releases/
@@ -43,4 +43,4 @@ appimage_arch="${appimage_arch/armv*/armhf}" # replaces "armv7l" with "armhf"
 if [ "$appimage_arch" = "x86" ]; then appimage_arch=i686; fi
 
 mkdir -p out
-mv src/runtime/runtime-fuse2 "out/runtime-fuse2-${appimage_arch}"
+mv src/runtime/runtime-fuse3 "out/runtime-fuse3-${appimage_arch}"
